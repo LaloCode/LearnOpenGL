845abd2b6f7ea67bddad6f54733319b570ea4bbc
unknown <lalo_pichu22@hotmail.com>
2021-05-07 17:10:25 -0500

    Adds color to each vertex

--- Getting started/SimpleWindow/Source.cpp
+++ Getting started/SimpleWindow/Source.cpp
@@ -10,16 +10,19 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"  ourColor = aColor;\n"  
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"  FragColor = ourColor;\n"
+"  FragColor = vec4(ourColor, 1.0);\n"
 "}\n";
 
 int main()
@@ -94,9 +97,9 @@ int main()
 
   // vertex data for triangles
   float triangle_vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+     0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+     0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
   }; 
 
   //unsigned int VAO, VBO, EBO;
@@ -113,9 +116,12 @@ int main()
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
 
-  // Tell OpenGL how to interpret the vertex data
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  // Position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  // Color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // render loop
   while (!glfwWindowShouldClose(window))
