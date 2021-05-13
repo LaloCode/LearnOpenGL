fa700b60306856ef858d92f8bed1f2349f2fccee
unknown <lalo_pichu22@hotmail.com>
2021-05-13 15:29:00 -0500

    Fix int casting in setFloat function

--- Getting started/SimpleWindow/Shader.cpp
+++ Getting started/SimpleWindow/Shader.cpp
@@ -93,5 +93,5 @@ void Shader::setInt(const std::string& name, int value) const {
   glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 void Shader::setFloat(const std::string& name, float value) const {
-  glUniform1f(glGetUniformLocation(ID, name.c_str()), (int)value);
+  glUniform1f(glGetUniformLocation(ID, name.c_str()), (float)value);
 }
